367136ffed209e21e7a7e63d5f683a7c3782d41d
Marco Crippa <m.crippa84@gmail.com>
2019-08-19 14:32:53 +0200

    add query

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+const axios = require('axios');
+
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.BOT_KEY;
 const url = process.env.APP_URL;
+const api_key = process.env.SEARCH_API_KEY;
+const search_engine_id = process.env.SEARCH_ID;
 
 const options = {
   webHook: {
@@ -11,12 +15,34 @@ const options = {
   }
 };
 
+// for deployment
 const bot = new TelegramBot(token, options);
 
+// for development
+// const bot = new TelegramBot(token, { polling: true });
+
+// for deployment
 bot.setWebHook(`${url}/bot${token}`);
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 bot.on('message', msg => {
   if (msg.text === 'gianni') {
-    bot.sendMessage(msg.chat.id, 'Ciao sono Gianni De Michelis');
+    axios
+      .get('https://www.googleapis.com/customsearch/v1', {
+        params: {
+          key: api_key,
+          cx: search_engine_id,
+          q: 'gianni de michelis',
+          searchType: 'image',
+          num: 100
+        }
+      })
+      .then(({ data }) =>
+        bot.sendPhoto(msg.chat.id, data.items[getRandomInt(100)].link)
+      )
+      .catch(err => console.log(err));
   }
 });
