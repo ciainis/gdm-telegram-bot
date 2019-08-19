51cb6ad7c89700144daa4656daccf4f6aab8e5ed
Marco Crippa <m.crippa84@gmail.com>
2019-08-19 12:40:18 +0200

    make it better

---
+++ index.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const TelegramBot = require('node-telegram-bot-api');
+const token = process.env.BOT_KEY;
+const url = process.env.APP_URL;
+
+const options = {
+  webHook: {
+    port: process.env.PORT
+  }
+};
+
+const bot = new TelegramBot(token, options);
+
+bot.setWebHook(`${url}/bot${token}`);
+
+bot.on('message', msg => {
+  bot.sendMessage(msg.chat.id, 'Ciao sono Gianni De Michelis');
+});
