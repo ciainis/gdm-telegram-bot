5e7b2a65194dce25d121b7c877bfbdc673dd4579
ciainis <m.crippa84@gmail.com>
2021-01-18 13:08:43 +0100

    oldify

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const { BOT_KEY, APP_URL, AC_CHAT_ID, MY_CHAT_ID } = process.env;
 const {
   getRandomInt,
   getRandomPhrase,
-  getRandomPhoto
+  getRandomPhoto,
 } = require('./functions');
 
 const getRandomOpening = require('./openings');
 
 const options = {
   webHook: {
-    port: process.env.PORT
-  }
+    port: process.env.PORT,
+  },
 };
 
 // for development
@@ -26,23 +26,25 @@ const bot = new TelegramBot(BOT_KEY, options);
 bot.setWebHook(`${APP_URL}/bot${BOT_KEY}`);
 
 const sendPhoto = async (chatId, text) => {
-  const searchText = text
-    .split(' ')
-    .splice(2)
-    .join(' ');
+  const searchText = text.split(' ').splice(2).join(' ');
 
   bot.sendPhoto(chatId, await getRandomPhoto(searchText));
 };
 
-const saySomething = async (chatId, user = null) => {
-  const phrase = await getRandomPhrase();
+const saySomething = async (chatId, user = null, old = false) => {
+  let phrase;
 
-  user
-    ? bot.sendMessage(chatId, `${getRandomOpening()} ${user}. ${phrase}`)
-    : bot.sendMessage(chatId, phrase);
+  const random = Math.random();
+  if (random >= 0.1 && old) {
+    phrase = 'OLD';
+  } else {
+    prase = await getRandomPhrase();
+  }
+
+  user ? bot.sendMessage(chatId, phrase) : bot.sendMessage(chatId, phrase);
 };
 
-bot.on('text', msg => {
+bot.on('text', (msg) => {
   if (
     msg.entities &&
     msg.entities[0].type === 'mention' &&
@@ -54,11 +56,11 @@ bot.on('text', msg => {
     msg.entities[0].type === 'mention' &&
     msg.text.toLowerCase().includes('@giannidm_bot')
   ) {
-    saySomething(msg.chat.id, msg.from.first_name);
+    saySomething(msg.chat.id, msg.from.first_name, true);
   }
 });
 
-bot.on('polling_error', err => console.log(err));
+bot.on('polling_error', (err) => console.log(err));
 
 setInterval(() => {
   const randomInt = getRandomInt(5);
