56758a9dfacaf42f0451de183a07f7c345e6efdf
ciainis <m.crippa84@gmail.com>
2021-03-13 17:45:22 +0100

    cleanup on text

--- index.js
+++ index.js
@@ -6,29 +6,14 @@ const { getRandomInt } = require('./helpers');
 const { sendPhoto, saySomething, sayOld } = require('./functions');
 const gianni = require('./gianni');
 
-gianni.on('text', (msg) => {
-  if (
-    msg.entities &&
-    msg.entities[0].type === 'mention' &&
-    msg.text.toLowerCase().startsWith('@giannidm_bot foto')
-  ) {
-    sendPhoto(msg.chat.id, msg.text);
-  } else if (
-    msg.entities &&
-    msg.entities[0].type === 'mention' &&
-    msg.text.toLowerCase().includes('@giannidm_bot')
-  ) {
-    saySomething(msg.chat.id, msg.from.first_name);
-  } else {
-    sayOld(msg.chat.id);
+gianni.on('text', (msg) => {});
+
+setInterval(() => {
+  const randomInt = getRandomInt(5);
+
+  if (randomInt === 1) {
+    saySomething(AC_CHAT_ID);
   }
-});
+}, 6000000);
 
 gianni.on('polling_error', (err) => console.log(err));
-
-// setInterval(() => {
-//   const randomInt = getRandomInt(5);
-//   if (randomInt === 1) {
-//     saySomething(AC_CHAT_ID);
-//   }
-// }, 600000);
